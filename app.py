2d296d9ef7e4c1d44a38fb0b9a52c0dde0b357b8
unknown <nodelwiche@gmail.com>
2020-10-17 19:44:37 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+class nyt_solver():
+    def __init__(self, essential, all):
+        self.essential = essential
+        self.all = all
+        with open('words.txt', encoding='utf-8') as f:
+            self.words = {x.lower().strip() for x in f.readlines()}
+
+    def solve(self):
+        return sorted({x for x in self.words if self.essential in x and not (set(x) - self.all)})
+
+
+app = Flask(__name__)
+@app.route("/<letters>")
+def home(letters):
+    solver = nyt_solver(essential=letters[0].lower(), all={x.lower() for x in letters})
+    return render_template('index.html', solved=solver.solve())
+
+if __name__ == '__main__':
+    app.run(debug=True)
